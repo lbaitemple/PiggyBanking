6c1b82b82adb010755e1d8f59a0fcd988881608d
Li Bai <lbai@temple.edu>
2023-12-17 17:39:37 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -14,25 +14,40 @@ login_manager.login_view = 'login'
 dynamodb = boto3.resource('dynamodb', region_name='us-east-2')
 table = dynamodb.Table('zany-pink-abalone-gearCyclicDB')
 
+
+
+
+
 class User(UserMixin):
     def __init__(self, user_id, username, password):
         self.id = user_id
         self.username = username
         self.password = password
 
+    @classmethod
+    def get(cls, user_id):
+        try:
+            response = table.get_item(Key={'id': user_id})
+            user_item = response.get('Item')
+            if user_item:
+                return User(user_item['id'], user_item['username'], user_item['password'])
+            return None
+        except Exception as e:
+            print(f"Error retrieving user: {e}")
+            return None
+
+        
+
 @login_manager.user_loader
 def load_user(user_id):
-    response = table.get_item(Key={'id': user_id})
-    user_item = response.get('Item')
-    if user_item:
-        return User(user_item['id'], user_item['username'], user_item['password'])
-    return None
+    return User.get(user_id)
+
 
 @app.route("/logout")
 @login_required
 def logout():
-    logout_user()
     return redirect(url_for('home'))
+    logout_user()
 
 @app.route("/")
 def home():
@@ -41,7 +56,68 @@ def home():
     else:
         return redirect(url_for('login'))
 
-# Other routes...
+
+@app.route("/about")
+def about():
+    return render_template("pages/about.html")
+
+@app.route("/overview")
+def overview():
+    return render_template("pages/overview.html")
+
+@app.route("/lesson1.html")
+def lesson1():
+    return render_template("pages/lesson1.html") 
+
+@app.route("/lesson2.html")
+def lesson2():
+    return render_template("pages/lesson2.html") 
+
+@app.route("/lesson3.html")
+def lesson3():
+    return render_template("pages/lesson3.html") 
+
+@app.route("/lesson4.html")
+def lesson4():
+    return render_template("pages/lesson4.html") 
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        
+        user = User.query.filter_by(username=username).first()
+        
+        if user and user.password == password:
+            login_user(user)
+            return redirect(url_for('about'))
+        else:
+            return 'Login Failed'
+
+    return render_template('login.html')
+
+@app.route("/register", methods=["GET", "POST"])
+@app.route("/register/", methods=["GET", "POST"])
+def register():
+    print("Register route called")  # Add this line for debugging
+
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        
+        # Check if the username is already taken
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user:
+            return 'Username already taken'
+        
+        new_user = User(username=username, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        
+        return redirect(url_for('login'))
+    
+    return render_template('register.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
